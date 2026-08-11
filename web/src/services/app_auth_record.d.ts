a3912738359665bf5b63696797df399e3a322005
cloudy <272685110@qq.com>
2026-08-11 15:07:57 +0800

    feat: 优化忽略文件，前端不使用gitlink

---
+++ web/src/services/app_auth_record.d.ts
@@ -0,0 +1,56 @@
+// AppAuthRecordCreate 应用认证记录创建
+export type AppAuthRecordCreate = { 
+  //应用ID
+  //最大长度: 50
+  applicationId?: string;
+  //响应编码 返回给浏览器客户端
+  //最大长度: 50
+  code: string;
+  //PKCE挑战值
+  //最大长度: 255
+  codeChallenge?: string;
+  //PKCE挑战方式
+  //最大长度: 20
+  codeChallengeMethod?: string;
+  //用户ID
+  //最大长度: 50
+  userId: string;
+  //OIDC nonce
+  //最大长度: 255
+  nonce?: string;
+}; 
+// AppAuthRecordDetail 应用认证记录详情
+export type AppAuthRecordDetail = { 
+  //主键
+  //最大长度: 50
+  id: string;
+  //创建时间
+  createdAt: string;
+  //创建时间
+  updatedAt: string;
+  //应用ID
+  //最大长度: 50
+  applicationId?: string;
+  //响应编码 返回给浏览器客户端
+  //最大长度: 50
+  code: string;
+  //PKCE挑战值
+  //最大长度: 255
+  codeChallenge?: string;
+  //PKCE挑战方式
+  //最大长度: 20
+  codeChallengeMethod?: string;
+  //用户ID
+  //最大长度: 50
+  userId: string;
+  //OIDC nonce
+  //最大长度: 255
+  nonce?: string;
+}; 
+// AppAuthRecordDetailList 应用认证记录列表响应
+export type AppAuthRecordDetailList = { 
+  //当前页数据
+  data?: AppAuthRecordDetail[];
+  //数据库满足条件的数据总数
+  total: number;
+}; 
---
+++ web/src/services/dashboard.d.ts
@@ -0,0 +1,29 @@
+//用户角色
+export type ApplicationAuthTop = { 
+  name?: string;
+  code?: string;
+  //最大长度: 50
+  applicationId?: string;
+  value?: number;
+  home?: string;
+  description?: string;
+  //默认值: tenant
+  //最大长度: 50
+  scope: string;
+}; 
+// Dashboard 系统统计数据
+export type Dashboard = { 
+  //人脸数据
+  faceRecognition?: number;
+  //认证方式
+  authProfile?: any;//todo 可能需要手动完善结构[];
+  //普通应用数据
+  application?: any;//todo 可能需要手动完善结构[];
+  //用户角色
+  userRole?: any;//todo 可能需要手动完善结构[];
+}; 
+// DashboardData 看板数据
+export type DashboardData = { 
+  name: string;
+  value: any;
+}; 
